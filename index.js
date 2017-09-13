e0bd624116f71ddcaae996d3c07e64ef39e9455f
kc657 <kvn.0218@gmail.com>
2017-09-12 23:30:00 -0700

    refactored the server file and working

--- index.js
+++ index.js
@@ -1,51 +1,39 @@
 const express = require('express')
-const http = require('http')
-const path = require('path')
-const fs = require('fs')
-const mime = require('mime')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
-const cfenv = require('cfenv')
-
-const vcapServices = require('vcap_services')
-
-const appEnv = cfenv.getAppEnv()
 const app = express()
+const port = process.env.PORT || 6003
 
-app.use(cookieParser())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-app.set('appName', 'kc-final')
-app.set('port', appEnv.port)
-
-app.set('views', path.join(__dirname + '/HTML'))
-app.engine('html', require('ejs').renderFile)
-app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/HTML'))
+
+// to config API to use body body-parser and look for JSON in req.body
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 app.use(bodyParser.json())
+// to config API to use cookieParser
+app.use(cookieParser())
 
-// Define your own router file in controller folder, export the router, add it into the index.js.
-// app.use('/', require("./controller/yourOwnRouter"));
+// Prevent CORS errors
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers')
 
+  // Remove caching
+  res.setHeader('Cache-Control', 'no-cache')
+  next()
+})
+
+// HOME
+app.get('/', function homepage (req, res) {
+  res.sendFile(__dirname + '/HTML/index.html')
+})
+
+// Controller Routes
 app.use('/', require('./controller/restapi/router'))
 
-http.createServer(app).listen(app.get('port'),
-    function (req, res) {
-      console.log(app.get('appName') + ' is listening on port: ' + app.get('port'))
-    })
-
-function loadSelectedFile (req, res) {
-  let uri = req.originalUrl
-  let filename = __dirname + '/HTML' + uri
-  fs.readFile(filename,
-        function (err, data) {
-          if (err) {
-            res.writeHead(500)
-            console.log('Error loading ' + filename + ' error: ' + err)
-            return res.end('Error loading ' + filename)
-          }
-          res.setHeader('content-type', mime.lookup(filename))
-          res.writeHead(200)
-          res.end(data)
-        })
-}
+app.listen(port, function () {
+  console.log(`App running on ${port}`)
+})
